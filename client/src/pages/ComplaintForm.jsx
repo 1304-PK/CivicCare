df98c3a14abc5ee6ae1cd0d091855404a59265db
PK-1304 <pushkark1304@gmail.com>
2026-01-13 09:56:32 +0530

    send form data and image to server

--- client/src/pages/ComplaintForm.jsx
+++ client/src/pages/ComplaintForm.jsx
@@ -1,29 +1,67 @@
-import { use, useRef } from "react"
+import { useState, useRef } from "react"
 
 const ComplaintForm = () => {
 
-    const subject_ref = useRef(null)
-    const desc_ref = useRef(null)
-    const location_ref = useRef(null)
-    const image_ref = useRef(null)
+  const [formFields, setFormFields] = useState({
+    subject: '',
+    description: '',
+    location: ''
+  })
 
-    const handleFormSubmit = (e) => {
+  const [file, setFile] = useState(null)
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target
+    setFormFields({
+      ...formFields,
+      [name]:value
+    }) 
+  }
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0])
+  }
+
+    const handleFormSubmit = async (e) => {
         e.preventDefault()
-        console.log(image_ref.current.files)
-            
+        const formData = new FormData()
+        formData.append('subject', formFields.subject)
+        formData.append('description', formFields.description)
+        formData.append('location', formFields.location)
+        formData.append('file', file)
+
+        try{
+          const res = await fetch("http://localhost:3000/upload-complaint", {
+            method: "POST",
+            body: formData
+          })
+          if (res.ok){
+            console.log("image received");
+          }
+          else{
+            console.log("image not received");
+          }
+        } catch(err){
+          console.log(err.message)
+        }
     }
 
   return (
     <div>
         <form onSubmit={handleFormSubmit}>
             <label htmlFor="subject">Subject</label>
-            <input type="text" name="subject" id="subject" ref={subject_ref}/>
+            <input type="text" name="subject" id="subject" onChange={handleInputChange}/>
             <label htmlFor="description">Description</label>
-            <textarea name="description" id="description" ref={desc_ref}></textarea>
+            <textarea name="description" id="description" onChange={handleInputChange}></textarea>
             <label htmlFor="location">Location</label>
-            <input type="text" name="location" id="location" ref={location_ref}/>
+            <input type="text" name="location" id="location" onChange={handleInputChange}/>
             <label htmlFor="image">Image</label>
-            <input type="file" name="image" accept="images/*" ref={image_ref}/>
+            <input 
+            type="file" 
+            name="image" 
+            accept="images/*"
+            onChange={handleFileChange}
+            />
             <button type="submit">Submit</button>
         </form>
     </div>

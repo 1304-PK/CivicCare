213a4abece143c6aa832f6b6460fd5e3c2937b17
PK-1304 <pushkark1304@gmail.com>
2026-01-15 02:12:37 +0530

    update router paths

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import SignUp from './pages/SignUp'
+import LogIn from './pages/LogIn'
+import UserDashboard from './pages/UserDashboard'
+import ComplaintForm from './pages/ComplaintForm'
 import './index.css'
-import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+  {
+    path:"/signup",
+    element: <SignUp />
+  },
+  {
+    path:"/login",
+    element: <LogIn />
+  },
+  {
+    path: "/user-dashboard",
+    element: <UserDashboard />
+  },
+  {
+    path: "/complaint-form",
+    element: <ComplaintForm />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

99d71661e4721cd5ef1a65676d91f59bbe33e243
PK-1304 <pushkark1304@gmail.com>
2026-01-20 10:06:15 +0530

    minor tweaks

--- client/src/components/AuthForm.jsx
+++ client/src/components/AuthForm.jsx
@@ -1,30 +1,31 @@
 import React, { useState } from 'react';
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import "../styles/AuthForm.css";
-import polka from "..//assets/polka.webp";
+import carousel1 from "../assets/carousel-1.webp"
+import carousel2 from "../assets/carousel-2.jpg"
+import carousel3 from "../assets/carousel-3.png"
 
 const AuthForm = (props) => {
   return (
     <div className="container">
       <div
         className="leftSection"
-        // style={{ backgroundImage: `url(${polka})` }}
       >
-        <div className="formWrapper">
+        <form className="formWrapper" onSubmit={props.handleSubmit}>
           <h1 className="formHeading">{props.type}</h1>
 
           <div className="signupForm">
             <div className="formGroup">
               <label className="label" htmlFor="name">Name</label>
-              <input type="text" name="username" ref={props.username_ref} id="username" className='input'/>
+              <input type="text" name="username" ref={props.username_ref} id="username" className='input' required/>
             </div>
 
             <div className="formGroup">
               <label className="label" htmlFor="password">Password</label>
-              <input type="password" ref={props.pswd_ref} name="password" id="password" className='input'/>
+              <input type="password" ref={props.pswd_ref} name="password" id="password" className='input' required/>
             </div>
 
-            <button className="signupButton" onClick={props.handleSubmit}>
+            <button className="signupButton" type='submit'>
               {props.type}
             </button>
           </div>
@@ -33,18 +34,59 @@ const AuthForm = (props) => {
             {props.redirect_text}{" "}
             <NavLink to={props.redirect_link} className="loginLink">{props.redirect}</NavLink>
           </p>
-        </div>
+        </form>
       </div>
 
       <div className="rightSection">
         <div className="rightContent">
-          <h2 className="rightHeading">Welcome to Nature Connect</h2>
-          <p className="rightDescription">
-            Join our community of nature enthusiasts and eco-conscious individuals. 
-            Discover sustainable living tips, connect with like-minded people, and 
-            make a positive impact on our planet. Start your journey towards a 
-            greener lifestyle today.
-          </p>
+          <div className='carousel-container'>
+            <div className="carousel">
+              <div className="carousel-track-down">
+                <div className="carousel-card">
+                  <img src={carousel1} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel2} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel3} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel1} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel2} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel3} alt="" />
+                </div>
+              </div>
+            </div>
+
+            <div className="carousel">
+              <div className="carousel-track-up">
+                <div className="carousel-card">
+                  <img src={carousel1} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel2} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel3} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel1} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel2} alt="" />
+                </div>
+                <div className="carousel-card">
+                  <img src={carousel3} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <h2 className="rightHeading">Fix the issues in your locality</h2>
         </div>
       </div>
     </div>

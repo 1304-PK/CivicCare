e305e4c6d128d41d483696a013ce9737fcede620
PK-1304 <pushkark1304@gmail.com>
2026-01-20 10:06:52 +0530

    minor tweaks

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react"
+import { Menu, X, LayoutDashboard, FileText, Clock, Settings, Plus } from 'lucide-react';
+import { useNavigate } from "react-router-dom";
+import "../styles/Sidebar.css"
+
+const Sidebar = () => {
+
+    const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
+    const navigate = useNavigate()
+
+    return (
+        <div className="sidebar">
+            <button
+        className="hamburger-toggle"
+        onClick={() => setIsMobileSidebarOpen(!isMobileSidebarOpen)}
+      >
+        {isMobileSidebarOpen ? <X size={20} /> : <Menu size={20} />}
+      </button>
+
+      {/* Sidebar Overlay */}
+      <div
+        className={`sidebar-overlay ${isMobileSidebarOpen ? 'active' : ''}`}
+        onClick={() => setIsMobileSidebarOpen(false)}
+      />
+
+      {/* Sidebar */}
+      <aside className={`sidebar ${isMobileSidebarOpen ? 'mobile-open' : ''}`}>
+        <div className="sidebar-content">
+          <div className="logo-section">
+            <h1 className="logo">CivicCare</h1>
+          </div>
+
+          <nav className="nav-menu">
+            <button className="nav-button-collapsed active">
+              <LayoutDashboard size={20} />
+            </button>
+            <button className="nav-button-expanded active" onClick={() => {navigate("/user-dashboard")}}>
+              <LayoutDashboard size={20} />
+              <span>Dashboard</span>
+            </button>
+
+            <button className="nav-button-collapsed">
+              <FileText size={20} />
+            </button>
+            <button className="nav-button-expanded" onClick={() => {navigate("/complaint-form")}}>
+              <FileText size={20} />
+              <span>New Complaint</span>
+            </button>
+
+            <button className="nav-button-collapsed">
+              <Clock size={20} />
+            </button>
+            <button className="nav-button-expanded" onClick={() => {navigate("/past-complaints")}}>
+              <Clock size={20} />
+              <span>Past Complaints</span>
+            </button>
+
+            <button className="nav-button-collapsed">
+              <Settings size={20} />
+            </button>
+            <button className="nav-button-expanded">
+              <Settings size={20} />
+              <span>Settings</span>
+            </button>
+          </nav>
+        </div>
+      </aside>
+        </div>
+    )
+}
+
+export default Sidebar

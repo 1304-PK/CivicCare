7744d65c53f5020f0a396dc690f06f12734bb9ba
PK-1304 <pushkark1304@gmail.com>
2026-01-26 02:47:50 +0530

    separate main.jsx file and routers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,41 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-import SignUp from './pages/SignUp'
-import LogIn from './pages/LogIn'
-import UserDashboard from './pages/UserDashboard'
-import ComplaintForm from './pages/ComplaintForm'
-import PastComplaints from './pages/PastComplaints'
-import Sidebar from './components/SideBar'
+import {RouterProvider} from 'react-router-dom'
+import router from "./router.jsx"
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />
-  },
-  {
-    path:"/signup",
-    element: <SignUp />
-  },
-  {
-    path:"/login",
-    element: <LogIn />
-  },
-  {
-    path: "/user-dashboard",
-    element: <UserDashboard />
-  },
-  {
-    path: "/complaint-form",
-    element: <ComplaintForm />
-  },
-  {
-    path: "/past-complaints",
-    element: <PastComplaints />
-  }
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

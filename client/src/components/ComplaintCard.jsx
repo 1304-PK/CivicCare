185739d62ceffaf156d519a2126b1f7b16faffe4
PK-1304 <pushkark1304@gmail.com>
2026-01-26 02:36:33 +0530

    handle status change and separate functionality for citizens and officers

--- client/src/components/ComplaintCard.jsx
+++ client/src/components/ComplaintCard.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import "../styles/ComplaintCard.css"
 import { MoreVertical, CheckCircle, Clock, Trash2 } from 'lucide-react';
+import capitalize from "../utils/capitalize.js"
 
-const ComplaintCard = ({ complaint, onDelete }) => {
+const ComplaintCard = ({ complaint, onDelete, officer, citizen, handleStatusChange }) => {
   const [showMenu, setShowMenu] = useState(false);
   const filePath = complaint.file_path.replace(/\\/g, '/')
   const imageUrl = `http://localhost:3000/${filePath}`
-  console.log(imageUrl)
+
   return (
     <div className="complaint-card">
       <div className="card-image-container">
-        <img src={imageUrl} alt={complaint.subject} className="card-image" />
-        <button 
+        <img src={imageUrl} alt={capitalize(complaint.subject)} className="card-image" />
+        {citizen && 
+        <>
+          <button 
           className="menu-button"
           onClick={() => setShowMenu(!showMenu)}
         >
@@ -31,19 +34,26 @@ const ComplaintCard = ({ complaint, onDelete }) => {
             </button>
           </div>
         )}
+        </>
+        }
       </div>
       <div className="card-content">
-        <h3 className="card-title">{complaint.subject}</h3>
-        <p className="card-description">{complaint.description}</p>
-        <p className="card-address">{`${complaint.address}, ${complaint.city}, ${complaint.state}`}</p>
-        <div className={`status-badge ${complaint.status}`}>
+        <h3 className="card-title">{capitalize(complaint.subject)}</h3>
+        <p className="card-description">{capitalize(complaint.description)}</p>
+        <p className="card-address">{`${capitalize(complaint.address)}, ${capitalize(complaint.city)}, ${capitalize(complaint.state)}`}</p>
+        <button className={`status-badge ${complaint.status}`}
+        onClick={officer ? 
+          () => {handleStatusChange(complaint.complaint_id, complaint.status)}
+          : () => {}}
+          style={officer && {cursor: "pointer"}}
+        >
           {complaint.status === 'resolved' ? (
             <CheckCircle size={16} />
           ) : (
             <Clock size={16} />
           )}
-          <span>{complaint.status === 'resolved' ? 'Resolved' : 'Pending'}</span>
-        </div>
+          {capitalize(complaint.status)}
+        </button>
       </div>
     </div>
   );
